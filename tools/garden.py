abc21fb43678c8ef3ea4e31e6537f38a1aec40d5
Jan Fecht <mail@fecht.cc>
2022-05-08 20:25:33 +0200

    Implement map matching and plotting

---
+++ tools/garden.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import os
+import os.path
+import time
+from pprint import pprint
+
+import folium
+import folium.plugins
+import matplotlib.pyplot as plt
+import osmnx as ox
+
+from gpstrack import GPSTrack,myHash
+
+DEFAULT_PLOTS_DIR = "plots"
+PLACES = ["Englischer Garten", "Isarinsel Oberföhring", "Wehranlage Oberföhring"]
+
+# don't put regex stuff in here
+
+HIGHWAY_BLACKLIST = ["service", "trunk", "trunk_link"]
+HIGHWAY_WHITELIST = [ht for ht in ['footway',
+ 'cycleway',
+ 'residential',
+ 'steps',
+ 'service',
+ 'unclassified',
+ 'track',
+ 'trunk_link',
+ 'trunk',
+ 'path',
+ 'bridleway'] if ht not in HIGHWAY_BLACKLIST]
+
+custom_filter = f'[highway~"^({"|".join(list(HIGHWAY_WHITELIST))})$"]'
+
+#network_type = "all"
+
+place_str = "@".join(PLACES + [custom_filter])
+
+# we use this file for caching the map data
+FNAME = f"data/{myHash(place_str)}.graphml"
+
+TO_RUN_COLOR = "#d0d0d0"
+BGCOLOR = "#333333"
+
+
+
+
+def save_plot(fig, plotname, plotsdir, extension="pdf"):
+    fig.savefig(f"{plotsdir}/{plotname}.{extension}", format=extension, bbox_inches="tight")
+
+
+
+
+
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="""
+Plot graphs in English Garden of past tracks.
+""".strip())
+
+    parser.add_argument("--gpxdir", "-d", required=True, help="The directory containing the gpx files for your runs.")
+    parser.add_argument("--outdir", "-o", default=DEFAULT_PLOTS_DIR, help="The directory where the plots are produced in.")
+    parser.add_argument("--cachefile", "-c", default=None, help="Path to a file where map matching results are cached. File will be created if it does not exist yet")
+    parser.add_argument("--debug", default=False, action="store_true", help="The directory where the plots are produced in.")
+
+    return parser
+
+def setup(args):
+    # caching is done by this program itself
+    ox.config(use_cache=False)
+
+    # fetch graph
+    if not os.path.isfile(FNAME):
+        print(f"Couldn't find cache file {FNAME} - Downloading map...")
+        G = ox.graph_from_place(PLACES, custom_filter=custom_filter, retain_all=True,
+                                truncate_by_edge=False, simplify=False)
+        ox.save_graphml(G, filepath=FNAME)
+
+    # setup cachefile for valhalla requests
+    if args.cachefile is not None:
+        GPSTrack.set_cachefile(args.cachefile)
+
+
+def load_relevant_tracks(gpxdir):
+    gpxfiles = [os.path.join(gpxdir, f) for f in os.listdir(gpxdir) if f.endswith(".gpx")]
+
+    print(f"Found {len(gpxfiles)} gpxfiles")
+    tracks_tocheck = [GPSTrack.from_gpx(f) for f in gpxfiles]
+
+
+    # filter tracks
+    tracks = []
+    for track in tracks_tocheck:
+        if len(track.points) == 0:
+            print(f"Track {track.filepath} - '{track.name}' has no points, ignoring")
+            continue
+
+        if track.name != "München Laufen":
+            print(f"Track {track.filepath} - '{track.name}' not in Munich, ignoring")
+            continue
+
+        tracks.append(track)
+
+    return tracks
+
+def plot_html_by_highway(G, fname):
+    gdf_edges = ox.graph_to_gdfs(G, nodes=False)
+    hwytypes = list(set(gdf_edges["highway"].tolist()))
+
+    # define the colors to use for different edge types
+    colorlist = [
+            'skyblue',
+            'paleturquoise',
+            'orange',
+            'sienna',
+            'lightgreen',
+            'grey',
+            'lightskyblue'
+            'yellow',
+            'magenta',
+            'cyan',
+            'red',
+            'green',
+            'blue',
+            'black',
+        ]
+    hwy_colors = {hwytypes[i]:colorlist[i] for i in range(len(hwytypes))}
+
+    # return edge IDs that do not match passed list of hwys
+    def find_edges(G, hwys):
+        edges = []
+        for u, v, k, data in G.edges(keys=True, data='highway'):
+            check1 = isinstance(data, str) and data not in hwys
+            check2 = isinstance(data, list) and all([d not in hwys for d in data])
+            if check1 or check2:
+                edges.append((u, v, k))
+        return set(edges)
+
+
+    # first plot all edges that do not appear in hwy_colors's types
+    G_tmp = G.copy()
+    m = ox.plot_graph_folium(G_tmp, weight=5, color='white')
+
+    # then plot each edge type in hwy_colors one at a time
+    for hwy, color in hwy_colors.items():
+        G_tmp = G.copy()
+        G_tmp.remove_edges_from(find_edges(G_tmp, [hwy]))
+        if G_tmp.edges:
+            m = ox.plot_graph_folium(G_tmp,
+                                    graph_map=m,
+                                    popup_attribute='highway',
+                                    weight=5,
+                                    color=color)
+    m.save(fname)
+
+
+def plot_html_debug(G, fname):
+    G = ox.load_graphml(filepath=FNAME)
+    G = ox.utils_graph.get_undirected(G)
+    gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
+    gdf_edges['summary'] = gdf_edges.apply(lambda x:
+        f"{x['osmid']}, {x['highway']}, {x['access']}, {x['from']}, {x['to']}",
+                                           axis=1)
+    G_tmp = ox.graph_from_gdfs(gdf_nodes, gdf_edges)
+    m = ox.plot_graph_folium(G_tmp,
+                            tiles="CartoDB positron",
+                            fit_bounds=True,
+                            weight=3,
+                            popup_attribute="summary")
+    m.save(fname)
+
+
+
+def main():
+
+    parser = create_parser()
+    args = parser.parse_args()
+
+    setup(args)
+
+    outdir = f"{args.outdir}/{int(time.time())}"
+    print(f"Using {outdir} as the output directory")
+    os.makedirs(outdir, exist_ok=True)
+
+
+    # load tracks
+    tracks = load_relevant_tracks(args.gpxdir)
+
+
+    # load our graph
+    G = ox.load_graphml(filepath=FNAME)
+
+    # save whole graph as interactive html for debugging
+    if args.debug:
+        plot_html_debug(G, f"{outdir}/english_garden_infos.html")
+        plot_html_by_highway(G, f"{outdir}/english_garden_highway.html")
+
+
+    # match tracks with edges
+    matched_tracks = []
+    for track in tracks:
+        route = track.match_graph(G)
+
+        if len(route) == 0:
+            print(f"Track {track.filepath} - '{track.name}' not in English Garden, ignoring")
+        else:
+            matched_tracks.append((track, route))
+
+
+
+
+    runned_edges = set(a for t in matched_tracks for a in t[1])
+    to_run_edges = G.edges(keys=True) - runned_edges
+
+    # edges will keep track of the color for each edge for static plot
+    edges_color = ox.graph_to_gdfs(G, nodes=False)
+    # default: to run -> gray
+    edges_color["color"] = TO_RUN_COLOR
+
+    G_to_run = G.copy()
+    G_to_run.remove_edges_from(runned_edges)
+
+    G_runned = G.copy()
+    G_runned.remove_edges_from(to_run_edges)
+
+    # m will be the folium.Map object for html visualization
+    # plot to-run edges in gray
+    m = ox.plot_graph_folium(G_to_run,
+                             tiles="CartoDB positron",
+                             fit_bounds=True,
+                             weight=3,
+                             color=TO_RUN_COLOR,
+                             opacity=1.0)
+
+
+
+
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    for (i,t) in enumerate(matched_tracks):
+        track,route = t
+
+        # determine next color
+        next_color = colors[i % len(colors)]
+
+        # get graph with only the runned edges
+        to_remove = set(e for e in G.edges if e not in route)
+        G_track = G.copy()
+        G_track.remove_edges_from(to_remove)
+
+        # color edges in our color lookup
+        edges_ran = G_track.edges(keys=True)
+        edges_color.loc[edges_color.index.isin(edges_ran),"color"] = next_color
+
+        # add route info for interactive html popup
+        gdf_nodes, gdf_edges = ox.graph_to_gdfs(G_track)
+        gdf_edges['route_info'] = f"{track.date.strftime('%Y-%m-%d')}"
+        G_track = ox.graph_from_gdfs(gdf_nodes, gdf_edges)
+
+        # add graph to folium map
+        m = ox.plot_graph_folium(G_track,
+                                 graph_map=m,
+                                 weight=3,
+                                 color=next_color,
+                                 popup_attribute="route_info",
+                                 opacity=1.0)
+
+    # save folium html map
+    m.save(f"{outdir}/map.html")
+
+    # folium.plugins.Fullscreen(
+    #     position="topright",
+    #     title="Expand",
+    #     title_cancel="Exit Fullscreen",
+    #     force_separate_button=True,
+    # ).add_to(m)
+
+
+
+    if args.debug:
+        # visualize gps points and matched points
+        for (i,t) in enumerate(matched_tracks):
+            track,route = t
+            next_color = colors[i % len(colors)]
+            cs = [folium.Circle(point, radius=2, opacity=0.5, color=next_color)
+                    for point in track.points]
+            for c in cs:
+                c.add_to(m)
+
+            cs = [folium.Circle(point, radius=2, color=next_color)
+                    for point in track.matched_points]
+            for c in cs:
+                c.add_to(m)
+
+        m.save(f"{outdir}/map-debug.html")
+
+
+    # create and save static plot
+    fig,ax = plt.subplots(facecolor=BGCOLOR)
+    ax.set_facecolor(BGCOLOR)
+    ox.plot_graph(G,
+                  ax=ax,
+                  edge_linewidth=1,
+                  node_size=0,
+                  edge_color=edges_color["color"].tolist(),
+                  show=False
+                  )
+    save_plot(fig, "map", outdir)
+
+
+
+
+
+    # statistics:
+    #  - how much is the total length
+    #  - how much did i run already (absolute value and percentage)
+    #  - how much do i have to run yet (absolute value and percentage)
+    #  - how much was run "unnecessarily" (absolute value) TODO
+    # for each run:
+    #  - date TODO
+    #  - how much did i run in absolute values TODO
+    #  - how much of the run were new routes (percentage) TODO
+    #  - how much of total roads were "uncovered" (percentage) TODO
+
+
+    edges_total = ox.graph_to_gdfs(G, nodes=False)
+    edges_runned = ox.graph_to_gdfs(G_runned, nodes=False)
+    edges_to_run = ox.graph_to_gdfs(G_to_run, nodes=False)
+
+    total_length_meters = edges_total["length"].sum()
+    runned_length_meters = edges_runned["length"].sum()
+    to_run_length_meters = edges_to_run["length"].sum()
+
+    runned_percentage = 100 * runned_length_meters / total_length_meters
+    to_run_percentage = 100 * to_run_length_meters / total_length_meters
+
+    stats = {
+        "total_length_meters": total_length_meters,
+        "runned_length_meters": runned_length_meters,
+        "to_run_length_meters": to_run_length_meters,
+
+        "runned_percentage": runned_percentage,
+        "to_run_percentage": to_run_percentage,
+    }
+    print("Statistic:")
+    print("=========================================")
+    pprint(stats)
+
+    # dump to file
+    json.dump(stats, open(f"{outdir}/stats.json", "w"))
+
+    return locals()
+
+if __name__ == "__main__":
+   locvar = main()
+   globals().update(locvar)
---
+++ tools/gpstrack.py
@@ -0,0 +1,292 @@
+import json
+import os
+import re
+import time
+from collections import OrderedDict
+from datetime import datetime
+
+import osmnx as ox
+import requests
+from shapely.geometry import Point
+
+
+def myHash(text):
+  h=0
+  [(h := ((h*281^ord(ch)*997)&0xffffffff)) for ch in text]
+  return str(h)
+
+
+class GPSTrack:
+    trkpt_pattern = re.compile(r'<trkpt.*?</trkpt>', flags=re.DOTALL)
+    point_pattern = re.compile(r'<trkpt lat="([\d.-]+)" lon="([\d.-]+)">')
+    name_pattern = re.compile(r'<name>([^<]+)</name>')
+    date_pattern = re.compile(r'<time>([^<]+)</time>')
+    track_type_pattern = re.compile(r'<type>([^<]+)</type>')
+
+    cache_path = None
+    request_cache = None
+
+    def __init__(self, name, points, timestamps, track_type, date, filepath=None):
+        """Create new GPSTrack
+
+        :param name: the name of the gps track
+        :param points: list of (lat, long) floats
+        :param timestamps: list of datetime objects corresponding to the points
+        :param track_type: one of ["running"]
+        :param date: datetime object representing the time of the run
+        """
+        self.name = name
+        self.points = points
+        self.timestamps = timestamps
+        self.track_type = track_type
+        self.date = date
+        self.filepath = filepath
+
+    @classmethod
+    def from_gpx(cls, filepath):
+        with open(filepath) as f:
+            content = f.read()
+
+        name = cls.name_pattern.search(content).group(1)
+
+        m = cls.date_pattern.search(content)
+        if m is None:
+            date = None
+        else:
+            datestr = m.group(1)
+            try:
+                date = datetime.strptime(datestr, "%Y-%m-%dT%H:%M:%S.000Z")
+            except ValueError:
+                print(f"Failed to parse timestamp: {datestr}")
+                date = None
+
+        track_type = cls.track_type_pattern.search(content)
+
+
+        track_points = cls.trkpt_pattern.findall(content)
+
+        points = [cls.point_pattern.search(text) for text in track_points]
+        if any(p is None for p in points):
+            print(filepath)
+            breakpoint()
+        points = [point.groups() for point in points]
+        points = [tuple(float(p) for p in point) for point in points]
+
+        timestamps = [datetime.strptime(cls.date_pattern.search(text).group(1), "%Y-%m-%dT%H:%M:%S.000Z")
+                      for text in track_points]
+        return cls(name=name, points=points, timestamps=timestamps, track_type=track_type, date=date, filepath=filepath)
+
+
+    @classmethod
+    def set_cachefile(cls, filepath):
+        cls.cache_path = filepath
+
+        if os.path.isfile(filepath):
+            with open(filepath, "r") as f:
+                cls.request_cache = json.load(f)
+                # print(cls.request_cache.keys())
+
+    @classmethod
+    def flush_cachefile(cls):
+        if cls.cache_path is not None and cls.request_cache is not None:
+            dir = os.path.dirname(cls.cache_path)
+            if not os.path.isdir(dir):
+                os.makedirs(dir)
+
+            with open(cls.cache_path, "w") as f:
+                tmp = json.dumps(cls.request_cache)
+                f.write(tmp)
+
+    def __str__(self):
+        return f"""
+GPSTrack(name={self.name}, date={self.date}, type={self.track_type}, points={len(self.points)} points)
+        """.strip()
+
+    __repr__ = __str__
+
+
+
+    def match_graph(self, G):
+        """match route against edges in G using valhalla APIII
+
+        :param G: graph to match against
+        :returns: list of matched edges contained in G
+        """
+
+        # Query API
+
+        baseurl = os.environ.get("VALHALLA_URL")
+        url = f"{baseurl}/trace_attributes"
+        # shape = [{"lat": p[0], "lon": p[1], "type": "via"} for p in self.points]
+        # shape[0]["type"] = "break"
+        # shape[-1]["type"] = "break"
+        shape = [OrderedDict({"lat": p[0], "lon": p[1]}) for p in self.points]
+
+        d = OrderedDict()
+        d["shape"] = shape
+        d["costing"] = "pedestrian"
+        d["shape_match"] = "map_snap"
+        d["begin_time"] = self.timestamps[0].strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        d["durations"] = [(d-d_prev).total_seconds() for (d_prev,d) in zip(self.timestamps[:-1], self.timestamps[1:])]
+
+        headers = {'Content-type': 'application/json'}
+
+
+        if GPSTrack.request_cache is None:
+            GPSTrack.request_cache = {}
+
+        request_hash = myHash(json.dumps(d))
+        if request_hash not in GPSTrack.request_cache:
+            print("Sending request to Valhalla for map matchine")
+            r = requests.post(url, data=json.dumps(d), headers=headers)
+            if r.status_code != 200:
+                breakpoint()
+            GPSTrack.request_cache[request_hash] = r.text
+            GPSTrack.flush_cachefile()
+
+        text = GPSTrack.request_cache[request_hash]
+
+        res = json.loads(text)
+
+        # parse result
+        mp = [p for p in res["matched_points"] if p["type"] == "matched"]
+
+        # save for optional later use
+        self.matched_points = [(p['lat'],p['lon']) for p in mp]
+
+
+        edges = res["edges"]
+        edges_is = [p["edge_index"] for p in mp]
+
+        matched_edges = [edges[ei] for ei in edges_is]
+
+        gdf_edges = ox.graph_to_gdfs(G, nodes=False)
+
+        matched_graph_edges = []
+        matchcount = 0
+        for me in matched_edges:
+            osmid = me['way_id']
+            mask = gdf_edges["osmid"].apply(lambda x: (type(x) == int and osmid == x)
+                                                      or (type(x) == list and osmid in x))
+            filtered = gdf_edges[mask]
+            if len(filtered) > 0:
+                matchcount += 1
+            for index,_ in filtered.iterrows():
+                matched_graph_edges.append(index)
+
+        print(f"Matched {matchcount/len(matched_edges) * 100}% of edges")
+        return matched_graph_edges
+
+
+
+    @staticmethod
+    def fit_points_mapbox(points, timestamps):
+        # https://mapbox-mapbox.readthedocs-hosted.com/en/latest/mapmatching.html
+        from mapbox import MapMatcher
+        mm = MapMatcher()
+
+
+        # API only allows 100 points
+        history = []
+        for i in range(0, len(points), 100):
+            j = min(i + 100, len(points))
+            line = {
+                "type": "Feature",
+                "properties": {
+                    "coordTimes": [d.strftime("%Y-%m-%dT%H:%M:%S.000Z") for d in timestamps[i:j]]
+                },
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [[p[1],p[0]] for p in points[i:j]]
+                }
+            }
+
+            r = mm.match(line, profile="mapbox.walking")
+            assert(r.status_code == 200)
+
+            corrected = r.geojson()['features'][0]['geometry']['coordinates']
+            history += corrected
+
+            time.sleep(1)
+
+
+        return [(c[1],c[0]) for c in history]
+
+
+
+    def match_graph2(self, G, max_distance=10):
+        """Match the route points to the edges of graph G
+
+        :param G:
+        :param max_distance: maximum allowed distance of point in meters to an
+                             edge to count the edge
+        :returns: list of edges in G
+        """
+
+        # project graph and points
+        P = ox.project_graph(G)
+        points_p = []
+        for plat,plon in self.points:
+            point_p, _ = ox.projection.project_geometry(Point(plon,plat), to_crs=P.graph['crs'])
+            points_p.append(point_p)
+
+
+        # find neared edge for each point
+        es = ox.nearest_edges(P, [p.x for p in points_p], [p.y for p in points_p], return_dist=True)
+        #return es[0]
+
+
+        # make list of (edge, distance, point)
+        es = [(e,es[1][i],self.points[i]) for (i,e) in enumerate(es[0])]
+
+        gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
+
+        # remove edges too far away from measurement
+        es2 = []
+        for i,e in enumerate(es):
+            dist = e[1]
+            if dist < max_distance:
+                es2.append(e)
+
+        es3 = []
+
+
+        # A very hacky heuristic
+        prev_e = None
+        edge_streak = []
+        from collections import defaultdict
+        d = defaultdict(list)
+        MIN_STREAK = 3
+        for i,e in enumerate(es2 + [((-1,-1,-1),-1,(-1,-1))]):
+            if prev_e != None:
+                if prev_e[0] != e[0]:
+                    # if we measured MIN_STREAK or more points on the edge after each other
+                    # we confirm the edge
+                    if len(edge_streak) >= MIN_STREAK:
+                        es3 += edge_streak
+                    else:
+                        # to avoid path where we where only shortly close to them
+                        # but not really on the path, we check whether our measurements
+                        # where too "close" to an intersection
+                        l = []
+                        for check_e in edge_streak:
+                            length = gdf_edges.loc[check_e[0]]["length"]
+                            p = check_e[2]
+                            p0 = check_e[0][0]
+                            p1 = check_e[0][1]
+                            d0 = ox.distance.great_circle_vec(gdf_nodes.loc[p0]["y"],gdf_nodes.loc[p0]["x"],p[0],p[1])
+                            d1 = ox.distance.great_circle_vec(gdf_nodes.loc[p1]["y"],gdf_nodes.loc[p1]["x"],p[0],p[1])
+                            dist = min(d0,d1)
+                            l.append(dist/length)
+                            #d[e[0]].append(dist/length)
+                        if max(l) > 0.2:
+                            es3 += edge_streak
+
+                    edge_streak = []
+
+            edge_streak.append(e)
+            prev_e = e
+
+
+        route = [e[0] for e in es3]
+        return route
